d82c6a6ea31288c46cb8f90b8dc942faae699217
Seth Wax <sethlwax@gmail.com>
2018-04-16 21:38:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_reader :side_1, :side_2, :side_3
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    sides = [self.side_1, self.side_2, self.side_3].sort
+
+    case
+    when sides[0] >= sides[1] + sides[2] || sides[2] <= 0
+      raise TriangleError
+    when sides[0] > sides[1] && sides[1] > sides[2]
+      :scalene
+    when sides[0] == sides[2]
+      :equilateral
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
